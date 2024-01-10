51b84fad70961e9d1b7267f073168dac9eb2c6a5
MariiaHula <mariyagulaya@gmail.com>
2024-01-10 23:00:15 +0200

    added readme.md(markdown)

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-<div id="header" align="center">
-  <img src="https://media.giphy.com/media/vLlpbDafjgHystuJ0a/giphy.gif" width="100"/>
-</div>
-<h1 align="center">
-   Greetings, dear
-   USENSE Company
-  <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="30px"/>
-</h1>
+![Gif of Yoda-coda](https://media.giphy.com/media/vLlpbDafjgHystuJ0a/giphy.gif)
 
-##Test task:
-#####Program for checking the reliability of the password.
+# Greetings, dear USENSE Company ![greetings hand](https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif)
+
+## Test task: Program for checking the reliability of the password.
+
+### Comment to the completion:
+
+- Fully verifying a password can be achieved using a library (not possible through regex). However, for this task, it's an additional dependency that might not be necessary, in my opinion. Yet, I understand that this isn't sufficient for the complete utilization of Unicode characters.
+
+- The advantages of this implementation lie in the ability to even use emojis for passwords, which not only avoids restrictions but also enhances password security for the user.
--- src/app/components/password-strength/password-strength.component.ts
+++ src/app/components/password-strength/password-strength.component.ts
@@ -1,62 +1,46 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-password',
   standalone: true,
   imports: [CommonModule, RouterModule, FormsModule],
-  template: `
-    <section>
-     <div class="strength-wrapper">
- <div class="show-wrapper">
-  <input class="strength-input" type="{{ showPassword ? 'text' : 'password' }}" placeholder="Enter Password" (input)="checkPasswordStrength($event)" [(ngModel)]="password">
-  <button class="strength-button" 
-      [ngClass]="{ 'yellow-button': !showPassword, 'blue-button': showPassword }" 
-      (click)="togglePasswordVisibility()">
-    {{ showPassword ? 'Hide': 'Show' }} Password
-  </button>
-</div>
-  <div class="strength-section" [ngStyle]="{'background-color': passwordStrengthColors[0]}"></div>
-  <div class="strength-section" [ngStyle]="{'background-color': passwordStrengthColors[1]}"></div>
-  <div class="strength-section" [ngStyle]="{'background-color': passwordStrengthColors[2]}"></div>
-</div>
-    </section>
-  `,
-  styleUrls: ['./password-strength.component.scss']
+  templateUrl: './password-strength.component.html',
+  styleUrls: ['./password-strength.component.scss'],
 })
-    
 export class PasswordStrengthComponent {
+  password: string = '';
+  passwordStrengthColors: string[] = ['gray', 'gray', 'gray'];
+  showPassword: boolean = false;
 
-password: string = '';
-passwordStrengthColors: string[] = ['gray', 'gray', 'gray'];
-showPassword: boolean = false;
+  checkPasswordStrength(event: Event): void {
+    const password = (event.target as HTMLInputElement).value;
 
-checkPasswordStrength(event: Event): void {
-     
-const password = (event.target as HTMLInputElement).value;
-        
     if (password.length === 0) {
       this.passwordStrengthColors = ['gray', 'gray', 'gray'];
     } else if (password.length < 8) {
       this.passwordStrengthColors = ['red', 'red', 'red'];
     } else {
       const hasLetters = /[a-zA-Z]/.test(password);
-      const hasDigits = /[0-9]/.test(password);
-      const hasSymbols = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password);
+      const hasDigits = /[\d]/.test(password);
+      const hasSymbols = /[^A-Za-z\d]/.test(password);
 
       if (hasLetters && hasDigits && hasSymbols) {
         this.passwordStrengthColors = ['green', 'green', 'green'];
-      } else if ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols)) {
+      } else if (
+        (hasLetters && hasDigits) ||
+        (hasLetters && hasSymbols) ||
+        (hasDigits && hasSymbols)
+      ) {
         this.passwordStrengthColors = ['yellow', 'yellow', 'gray'];
       } else {
         this.passwordStrengthColors = ['red', 'gray', 'gray'];
       }
     }
   }
-    togglePasswordVisibility(): void {
+  togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
   }
 }
